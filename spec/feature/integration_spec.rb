a95fa2f986c0df565a0555c753de8bbc2c468be2
AmazingBrandonL <brandon.longuet@yahoo.com>
2023-02-13 21:11:26 -0600

    FIXED: Testing for the features

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -6,8 +6,13 @@ RSpec.describe 'Creating a user', type: :feature do
   scenario 'valid inputs' do
     visit new_user_path
     fill_in "user[full_name]", with: 'harry potter'
+    fill_in "user[email]", with: 'harrypotter@mybusiness.com'
+    fill_in "user[phone_number]", with: '0123456789'
     click_on 'Create User'
+    visit users_path
     expect(page).to have_content('harry potter')
+    expect(page).to have_content('harrypotter@mybusiness.com')
+    expect(page).to have_content('0123456789')
   end
 end
 
@@ -20,5 +25,18 @@ RSpec.describe 'Creating a event', type: :feature do
     click_on 'Create Event'
     visit events_path
     expect(page).to have_content('Wizarding Magic')
+  end
+end
+
+RSpec.describe 'Creating a group', type: :feature do
+  scenario 'valid inputs' do
+    visit new_group_path
+    fill_in "group[name]", with: 'Year 7 Hufflepuff'
+    fill_in "group[description]", with: 'Hufflepuff students in their seventh year at Hogwarts'
+    
+    click_on 'Create Group'
+    visit groups_path
+    expect(page).to have_content('Year 7 Hufflepuff')
+    expect(page).to have_content('Hufflepuff students in their seventh year at Hogwarts')
   end
 end
