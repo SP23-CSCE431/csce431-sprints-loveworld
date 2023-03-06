56f36d56c56fef4e4bae740042d37c17f0c75d84
raulescobar-g <raulescobar_g@tamu.edu>
2023-03-06 10:45:49 -0600

    Requested changes + fixed auth for tests

--- spec/feature/event_integration_spec.rb
+++ spec/feature/event_integration_spec.rb
@@ -2,6 +2,22 @@ require 'rails_helper'
 require 'capybara/rspec'
 
 RSpec.describe('Event') do
+  before do
+    Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+
+    visit '/'
+    click_on 'Sign in with Google'
+
+    fake_user = Rails.application.env_config['omniauth.auth']
+
+    visit new_user_path
+    fill_in 'user[full_name]', with: fake_user.info.name
+    fill_in 'user[email]', with: fake_user.info.email
+    fill_in 'user[phone_number]', with: '0123456789'
+    click_on 'Create User'
+  end
+
   context 'when using valid input' do
     it 'creates an event' do
       visit new_event_path
--- spec/feature/user_integration_spec.rb
+++ spec/feature/user_integration_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 require 'capybara/rspec'
 
-RSpec.describe('User') do
+RSpec.describe('User', type: :controller) do
+  before do
+    Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+
+    visit '/'
+    click_on 'Sign in with Google'
+
+    fake_user = Rails.application.env_config['omniauth.auth']
+
+    visit new_user_path
+    fill_in 'user[full_name]', with: fake_user.info.name
+    fill_in 'user[email]', with: fake_user.info.email
+    fill_in 'user[phone_number]', with: '0123456789'
+    click_on 'Create User'
+  end
+
   context 'when using valid input' do
     it 'creates an account' do
       visit new_user_path
