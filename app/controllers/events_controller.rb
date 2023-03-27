e1b5ee9c2a64768adad331f6855dc430d5587146
raulescobar-g <raulescobar_g@tamu.edu>
2023-03-26 21:50:35 -0500

    save

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,10 @@
+require "google/apis/calendar_v3"
+require "google/api_client/client_secrets.rb"
+
 class EventsController < ApplicationController
   before_action :set_event, only: %i[show edit update destroy]
+
+  CALENDAR_ID = 'primary'
 
   # GET /events or /events.json
   def index
@@ -8,7 +13,7 @@ class EventsController < ApplicationController
     @user_event_array = Event.select('id').joins(:event_members).where('event_members.user_id' => @current_id.id).to_a.map(&:id)
 
     user_email = ERB::Util.url_encode(current_admin.email)
-    @calendar_url = "https://calendar.google.com/calendar/embed?src=#{user_email}&ctz=America%2FChicago"
+    @calendar_url = "https://calendar.google.com/calendar/embed?src=c_6fa3a48d19f0d7d599da305fe3e3b26ca3ed3102a85a870552b0f4dbf80c0c07%40group.calendar.google.com&ctz=America%2FChicago"
   end
 
   # GET /events/1 or /events/1.json
@@ -28,6 +33,13 @@ class EventsController < ApplicationController
   # POST /events or /events.json
   def create
     @event = Event.new(event_params)
+
+    # create event in google calendar
+    client = get_google_calendar_client current_admin
+
+    google_event = create_google_event @event
+
+    client.insert_event('primary', google_event)
 
     respond_to do |format|
       if @event.save
@@ -73,5 +85,61 @@ class EventsController < ApplicationController
   # Only allow a list of trusted parameters through.
   def event_params
     params.require(:event).permit(:name, :start, :end)
+  end
+
+  def create_google_event event
+    event = Google::Apis::CalendarV3::Event.new(
+      summary: event.name,
+      location: '',
+      description: event.name,
+      start: Google::Apis::CalendarV3::EventDateTime.new(
+        date_time: event.start.to_s,
+        time_zone: 'America/Chicago'
+      ),
+      end: Google::Apis::CalendarV3::EventDateTime.new(
+        date_time: event.end.to_s,
+        time_zone: 'America/Chicago'
+      ),
+      recurrence: [
+        'RRULE:FREQ=DAILY;COUNT=1'
+      ],
+      attendees: []
+    )
+  end
+
+  def get_google_calendar_client current_user
+    client = Google::Apis::CalendarV3::CalendarService.new
+
+    puts current_admin.methods
+    puts "howdy ----"
+    access_token = "here"
+    refresh_token = "here"
+
+    return unless (current_user.present? && access_token.present? && refresh_token.present?)
+    secrets = Google::APIClient::ClientSecrets.new({
+      "web" => {
+        "access_token" => access_token,
+        "refresh_token" => refresh_token,
+        "client_id" => "AIzaSyBXzTYpS3QHGVI5cYrMx4vOy39Nh0Gqz6E", # TODO: replace
+        "client_secret" => "477074288964-jq2ouosnb87921jtofeuh23so0d0eej5.apps.googleusercontent.com" # TODO: replace
+      }
+    })
+    begin
+      client.authorization = secrets.to_authorization
+      client.authorization.grant_type = "refresh_token"
+
+      if !current_user.present?
+        client.authorization.refresh!
+        current_user.update_attributes(
+          access_token: client.authorization.access_token,
+          refresh_token: client.authorization.refresh_token,
+          expires_at: client.authorization.expires_at.to_i
+        )
+      end
+    rescue => e
+      flash[:error] = 'Your token has been expired. Please login again with google.'
+      redirect_to :back
+    end
+    client
   end
 end
